90ed765597f283f33d0233eda04ecb2f9a3eb6f0
Ali Waqas <aliwaqas@codeforpakistan.org>
2020-01-20 19:34:12 +0500

    .

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState,useEffect } from 'react';
+
+export default function Header(){
+    // console.log('Current directory: ' + process.cwd());
+    useEffect(() => {
+        
+    }, []);
+    return(
+        <>
+        <h1>This is header</h1>
+        {/* <img src="./images/korean-icons-737x500.jpg" alt="" /> */}
+        </>
+    )
+}
